22e5697ebc3e43cd3eab9cfaa14624a927d94214
rahul sharma <rraj25198@gmail.com>
2026-07-19 01:52:22 +0530

    Configure Eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import neostandard from "neostandard";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+ ...neostandard({ ts: true }),
+  prettierConfig,
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
