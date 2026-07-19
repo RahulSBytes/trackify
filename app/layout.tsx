738f9b74f60180ad4cb31c485d207a6ba92642f3
rahul sharma <rraj25198@gmail.com>
2026-07-19 11:20:33 +0530

    Set up font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono as GeistMono } from 'next/font/google'
+import localfont from 'next/font/local'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
+const inter = localfont({
+  src: './fonts/interVF.woff2',
+  variable: '--font-inter',
+  weight: '400 500 600'
 })
 
-const geistMono = GeistMono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
+const baumans = localfont({
+  src: './fonts/BaumansVF.woff2',
+  variable: '--font-baumans',
+  weight: '400'
 })
 
 export const metadata: Metadata = {
@@ -23,11 +25,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang='en'
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className='flex min-h-full flex-col'>{children}</body>
+    <html lang='en' className='h-full antialiased dark'>
+      <body
+        className={` ${inter.variable} ${baumans.variable} flex h-full min-h-full flex-col antialiased`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
